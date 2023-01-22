f2952c5da6cece17ade00063e7ac744cad7942cd
Victor Melo <contatomelo2@gmail.com>
2023-01-22 01:25:08 -0300

    update

--- script.js
+++ script.js
@@ -3,22 +3,24 @@ const nlwSetup = new NLWSetup(form)
 const button = document.querySelector('header button')
 
 button.addEventListener('click', add)
+form.addEventListener("change", save)
 
 function add() {
-
-    const today = "01/01"
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
     const dayExists = nlwSetup.dayExists(today)
 
     if(dayExists) {
-        alert("Dia já incluso. ❌")
+        alert("Dia já incluso.⚠")
         return
     }
-    
+
     alert('Adiconado com sucesso ✅')
     nlwSetup.addDay(today)
 }
 
-
+function save () {
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
 
 
 
